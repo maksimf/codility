9fab7e27a95d8fd09c685dfb123848e4b9ec2df3
Maksim Filippov <m.filippov@rambler-co.ru>
2015-09-28 11:50:37 +0300

    Add FrogRiverOne

---
+++ counting_elements/FrogRiverOne/frog_river_one.rb
@@ -0,0 +1,10 @@
+def solution(x, a)
+    steps = Set.new
+    for i in (0..(a.length-1)) do
+        steps.add a[i] if a[i] <= x
+
+        return i if steps.length == x
+    end
+
+    return -1
+end
